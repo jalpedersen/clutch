bc7951c0fc0c5522f673e7723c465a304ecb401f
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2014-02-19 21:13:42 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,25 +3,23 @@
   :url "https://github.com/clojure-clutch/clutch/"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  
-                 [clj-http "0.5.5"]
-                 [cheshire "4.0.0"]
-                 [commons-codec "1.6"]
-                 [com.cemerick/url "0.0.6"]
+                 [clj-http "0.7.9"]
+                 [cheshire "5.3.1"]
+                 [commons-codec "1.9"]
+                 [com.cemerick/url "0.1.1"]
                  
-                 [org.clojure/clojurescript "0.0-1450" :optional true
+                 [org.clojure/clojurescript "0.0-2156" :optional true
                   :exclusions [com.google.code.findbugs/jsr305
                                com.googlecode.jarjar/jarjar
                                junit
                                org.apache.ant/ant
-                               org.json/json
                                org.mozilla/rhino]]]
   :profiles {:dev {}
-             :1.2.0 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-             :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-alpha6"]]}}
-  :aliases  {"all" ["with-profile" "dev,1.2.0:dev,1.3.0:dev:dev,1.5.0"]}
+             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :aliases  {"all" ["with-profile" "dev:dev,1.5.0:dev,1.5.1"]}
   :min-lein-version "2.0.0"
   :test-selectors {:default #(not= 'test-docid-encoding (:name %))
                    :all (constantly true)})
