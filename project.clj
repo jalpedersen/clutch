cf944755c6be52af7f63c5a4247e975a312c40d9
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2014-02-28 22:17:15 +0100

    clj-http 0.9.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  
-                 [clj-http "0.7.9"]
+                 [clj-http "0.9.0"]
                  [cheshire "5.3.1"]
                  [commons-codec "1.9"]
                  [com.cemerick/url "0.1.1"]
